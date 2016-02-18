3ce0b5a2c13c9f5a1b4dc8a7715982a192eb35f2
Jeremy <Jeremy@74.74.163.70>
2016-02-18 00:05:05 -0500

    Worked on authentication, page guessing

--- Fuzzer.py
+++ Fuzzer.py
@@ -7,6 +7,11 @@ Jeremy Friedman
 Aaron Stadler
 """
 
+"""
+Partial validation. 
+@args userArgs -- user input
+@return: number of inputs
+"""
 def validateInput(userArgs):
     try:
         fuzzArg = userArgs[1] == "fuzz"
@@ -16,18 +21,60 @@ def validateInput(userArgs):
         commonWords = False
         if len(userArgs) == 5:
             commonWords = "--common-words=" in userArgs[4]
-            return (fuzzArg and fuzzMode and url and commonWords)
+            return (len(userArgs))
         elif len(userArgs) == 6:
             customAuth = "--custom-auth=" in userArgs[4]
             commonWords = "--common-words=" in userArgs[5]
-            return (fuzzArg and fuzzMode and url and customAuth and commonWords)
+            return (len(userArgs))
     except IndexError:
         print("***Invalid args*** \nUsage: fuzz discover <url> [--custom-auth=<string>] --common-words=<filename>")
         sys.exit()
 
+def guessPages(url, commonWords):
+    extensions = [".php", ".jsp"]
+    pages = []
+    for word in open(commonWords):
+        for extension in extensions:
+            request = requests.get(url + "/" + word.strip("\n") + extension)
+            if (request.status_code != 404):
+                pages.append('http://127.0.0.1/' + word.strip() + extension)
+    print(pages)
+
+"""
+Returns a requests.Response object
+"""
+def authenticate(authTo):
+    session = requests.session()
+    #hard-coded
+    if (authTo == "dvwa"):
+        pass
+    elif (authTo == "bodgeit"):
+        pass
+        
 
 def fuzz(userArgs):
-    validateInput(userArgs)
+    #validate input
+    numArgs = validateInput(userArgs)
+    
+    #init vars
+    customAuth = ""
+    url = userArgs[-2]
+    commonWords = userArgs[-1].split("=")[1]
+    if (numArgs == 6):
+        customAuth = userArgs[-2].split("=")[1]
+        url = userArgs[-3]
+
+    #discover
+    guessedPages = []
+    if (userArgs[2] == "discover"):
+        if (customAuth != ""):
+            authenticate(userArgs[4].split('=')[1])
+        guessedPages = guessPages(url, commonWords)
+    
+    elif (userArgs[2] == "test"):
+        pass #do test stuff
+        
+
 
 
 if __name__ == "__main__":
