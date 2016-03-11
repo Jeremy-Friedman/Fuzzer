73003bf5645fbfcedb57be7a73e14ac287769fb0
aaron13michael <ams8560@rit.edu>
2016-03-11 09:52:32 -0500

    Added class to populate form parameters and submit forms

--- submitForm.py
+++ submitForm.py
@@ -1,6 +1,6 @@
-'''
+"""
 Submits the form on a page with given parameters and returns the response
-'''
+"""
 import bs4
 import requests
 
@@ -8,11 +8,9 @@ import requests
 Generates form parameters by finding all the inputs and setting
 them to a given value
 '''
-def setParams(url, session, value):
-    html = session.get(url).text
-    soup = bs4.BeautifulSoup(html, "html.parser", parse_only = bs4.SoupStrainer("input"))
+def setFormParams(formTag, value):
     params = {}
-    for input in soup:
+    for input in formTag.find_all("input"):
         if not input.get('name') == None:
             params[input['name']] = value
     return params
@@ -21,13 +19,14 @@ Submits all the forms on the given page with
 the given parameters. HTTP Method is determined by
 method specified in form
 '''
-def submitForms(url, session, parameters):
+def submitForms(url, session, value):
     html = session.get(url).text
     soup = bs4.BeautifulSoup(html, "html.parser", parse_only = bs4.SoupStrainer("form"))
     responses = list()
     for form in soup:
         method = form['method'].upper()
         submiturl = url[0 : url.rfind("/")] + "/" + form['action']
+        parameters = setFormParams(form, value)
         response = session.request(method=method,url=submiturl, params=parameters)
         responses.append(response)
     return responses
