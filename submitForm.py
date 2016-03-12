777482bc941a0e4c0fa9225281fb82833ed44e3e
aaron13michael <ams8560@rit.edu>
2016-03-11 23:40:16 -0500

    Created file with methods to confirm vulnerabilities and added random submit to sumbit form

--- submitForm.py
+++ submitForm.py
@@ -3,6 +3,8 @@ Submits the form on a page with given parameters and returns the response
 """
 import bs4
 import requests
+import time
+import random
 
 '''
 Generates form parameters by finding all the inputs and setting
@@ -27,6 +29,34 @@ def submitForms(url, session, value):
         method = form['method'].upper()
         submiturl = url[0 : url.rfind("/")] + "/" + form['action']
         parameters = setFormParams(form, value)
+        start = time.time()
         response = session.request(method=method,url=submiturl, params=parameters)
+        end = time.time()
         responses.append(response)
     return responses
+
+'''
+Submit a value into a random input on the page
+'''
+def submitRandom(url, session, value):
+    params = {}
+    inputOptions = list()
+    html = session.get(url).text
+    soup = bs4.BeautifulSoup(html, "html.parser", parse_only = bs4.SoupStrainer("input"))
+    for input in soup:
+        if input.get('name') != None:
+            inputOptions.append(input)
+    if len(inputOptions) > 0:
+        ranInput = random.choice(inputOptions)
+        for parent in ranInput.parents:
+            if parent.name == 'form':
+                form = parent
+                break
+        if form:
+            params[ranInput['name']] = value
+            submiturl = url[0 : url.rfind("/")] + "/" + form['action']
+            return session.request(method=form['method'].upper(),url=submiturl, params=params)
+        else:
+            return False
+    else:
+        return False
